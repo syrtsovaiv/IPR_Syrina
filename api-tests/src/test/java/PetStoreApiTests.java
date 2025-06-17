5db59a04d45ad7a814467b055caca1c475f5dca5
syrtsovaiv <102823286+syrtsovaiv@users.noreply.github.com>
2025-06-17 15:49:20 +0300

    add api test

--- api-tests/src/test/java/PetStoreApiTests.java
+++ api-tests/src/test/java/PetStoreApiTests.java
@@ -1,43 +1,26 @@
 import io.restassured.RestAssured;
-
 import io.restassured.http.ContentType;
-
 import org.junit.jupiter.api.BeforeEach;
-
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.*;
-
 import static org.hamcrest.Matchers.*;
 
-public class PetStoreApiTests {
+public class PetStoreApiTests { //УБРАТЬ ПРОБЕЛЫ
 
     @BeforeEach
-
     public void setup() {
-
         RestAssured.baseURI = "https://petstore.swagger.io/v2";
-
     }
 
     @Test
-
     public void GetPetById() {
-
         given().
-
                 pathParam("petId", 3004).
-
                 when().
-
                 get("/pet/{petId}").
-
                 then().
-
                 statusCode(200).
-
                 body("id", equalTo(3004)).
-
                 body("name", notNullValue());
 
     }
@@ -47,25 +30,15 @@ public class PetStoreApiTests {
     public void CreatePet() {
 
         String newPet = "{ \"id\": 1001, \"name\": \"Doggie\", \"status\": \"available\" }";
-
         given().
-
                 contentType(ContentType.JSON).
-
                 body(newPet).
-
                 when().
-
                 post("/pet").
-
                 then().
-
                 statusCode(200).
-
                 body("id", equalTo(1001)).
-
                 body("name", equalTo("Doggie")).
-
                 body("status", equalTo("available"));
 
     }
@@ -77,21 +50,13 @@ public class PetStoreApiTests {
         String updatedPet = "{ \"id\": 1001, \"name\": \"UpdatedDoggie\", \"status\": \"sold\" }";
 
         given().
-
                 contentType(ContentType.JSON).
-
                 body(updatedPet).
-
                 when().
-
                 put("/pet").
-
                 then().
-
                 statusCode(200).
-
                 body("name", equalTo("UpdatedDoggie")).
-
                 body("status", equalTo("sold"));
 
     }
@@ -101,17 +66,11 @@ public class PetStoreApiTests {
     public void testDeletePet() {
 
         given().
-
                 pathParam("petId", 1001).
-
                 when().
-
                 delete("/pet/{petId}").
-
                 then().
-
                 statusCode(200).
-
                 body("message", equalTo("1001"));
 
     }
